87a2bec90c8e8661f78a5fe5b6dfb66b2f32232b
Modester_coding <nnamaninneamaka35@gmail.com>
2025-06-26 14:16:35 +0100

    Update script.js

--- project1/script.js
+++ project1/script.js
@@ -1,4 +1,6 @@
-
+// DOM Elements
+const startScreen = document.getElementById("start-screen");
+const quizScreen = document.getElementById("quiz-screen");
 const resultScreen = document.getElementById("result-screen");
 const startButton = document.getElementById("start-btn");
 const questionText = document.getElementById("question-text");
